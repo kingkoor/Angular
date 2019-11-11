0a97d5f770368a3304ecc9c3921f3a64eab57ca1
kingkoor <kingkoor@yahoo.com>
2019-11-11 08:33:34 -0800

    Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,14 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule} from '@angular/material/checkbox';
+
+import { MatSelectModule} from '@angular/material/select';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 //template driven form
 import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -51,6 +57,7 @@ import { LoginComponent } from './login/login.component';
     MatToolbarModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MatListModule,
     MatGridListModule,
@@ -59,7 +66,9 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   entryComponents: [
     LoginComponent
