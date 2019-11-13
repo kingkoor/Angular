c948fcb0cc7cb56987453a08d3e6ea1e61374c7d
kingkoor <kingkoor@yahoo.com>
2019-11-13 11:43:59 -0800

    add custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { ProcessHTTPMsgService} from './services/process-httpmsg.service';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { baseURL} from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -55,7 +56,8 @@ import { baseURL} from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
